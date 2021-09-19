27bd1e7d83a8dd3b7ca392744dadd72b60e0f017
Leo <liyung.chen@tum.de>
2021-09-19 02:12:06 +0200

    add CS programs: TUM_DATA_ENG TUBERLIN_CS FU_DATA RWTH_DATA_SCIENCE

--- database/ComputerScience/CS_KEYWORDS.py
+++ database/ComputerScience/CS_KEYWORDS.py
@@ -5,13 +5,17 @@ DIFFERENTIATE_KEY_WORDS = 2
 #################################################################################################################
 ############################################## Computer Science #################################################
 #################################################################################################################
-CS_INTRO_INFO_KEY_WORDS = ['計算機', '物件', '運算',
-                           '程式語言', '程式設計', 'C語言', 'JAVA']
+CS_INTRO_INFO_KEY_WORDS = ['計算機', '物件', '運算']
 CS_INTRO_INFO_ANTI_KEY_WORDS = ['倫理', '組織',
                                 '架構', '結構', '理論', '網路', 'MATLAB', 'Matlab']
+CS_INTRO_ELEKTROTECHNIK_KEY_WORDS = ['邏輯設計', '電子學', '電路學']
+CS_INTRO_ELEKTROTECHNIK_ANTI_KEY_WORDS = ['倫理', '組織', ]
+CS_PROGRAMMING_KEY_WORDS = ['程式語言', '程式設計', 'C語言', 'JAVA']
+CS_PROGRAMMING_ANTI_KEY_WORDS = ['倫理', '網頁', '視窗', '組織',
+                                 '架構', '結構', '理論', '網路', 'MATLAB', 'Matlab']
 CS_COMP_ARCH_KEY_WORDS = ['電腦結構', '計算機結構', '計算機架構', '計算機組織', '軟硬', '編譯',
                           '組合語言', '微算機', '數位電路', '硬體描述', 'VHDL', 'Verilog']
-CS_COMP_ARCH_ANTI_KEY_WORDS = ['asdgladfj']
+CS_COMP_ARCH_ANTI_KEY_WORDS = ['asdgladfj', '專題']
 CS_SWE_KEY_WORDS = ['軟體', '系統設計']
 CS_SWE_ANTI_KEY_WORDS = ['asdgladfj']
 CS_DB_KEY_WORDS = ['資料庫']
@@ -22,9 +26,13 @@ CS_COMP_NETW_KEY_WORDS = ['電腦網路', '網路', '密碼', '安全', '傳輸'
 CS_COMP_NETW_ANTI_KEY_WORDS = ['asdgladfj']
 CS_FUNC_PROG_KEY_WORDS = ['功能', '驗證', '函式語言', '函式程式', '函數編程']
 CS_FUNC_PROG_ANTI_KEY_WORDS = ['網頁']
+CS_MATH_LOGIC_KEY_WORDS = ['數學邏輯', '數理邏輯']
+CS_MATH_LOGIC_PROG_ANTI_KEY_WORDS = ['asdgladfj;l']
 CS_ALGO_DATA_STRUCT_KEY_WORDS = ['演算法', '資料結構']
 CS_ALGO_DATA_STRUCT_ANTI_KEY_WORDS = ['asdgladfj']
-CS_THEORY_COMP_KEY_WORDS = ['計算機理論', '原理', '正規', '常規']
+CS_FORMAL_METHOD_KEY_WORDS = ['正規', '常規', '形式系統']
+CS_FORMAL_METHOD_ANTI_KEY_WORDS = ['asdgladfj']
+CS_THEORY_COMP_KEY_WORDS = ['計算機理論', '原理', '可計算']
 CS_THEORY_COMP_ANTI_KEY_WORDS = ['asdgladfj']
 CS_MATH_DISCRETE_KEY_WORDS = ['離散', '圖形', '圖論', '邏輯']
 CS_MATH_DISCRETE_ANTI_KEY_WORDS = ['asdgladfj;l']
@@ -34,6 +42,9 @@ CS_CALCULUS_KEY_WORDS = ['微積分', '向量']
 CS_CALCULUS_ANTI_KEY_WORDS = ['asdgladfj;l']
 CS_MATH_PROB_KEY_WORDS = ['機率', '統計', '隨機']
 CS_MATH_PROB_ANTI_KEY_WORDS = ['asdgladfj;l']
+CS_ADVANCED_INFO_KEY_WORDS = ['語音', '推薦', '機器學習', '通訊原理', '自然語言', '高等', '網頁', '視窗',
+                              '專題', '嵌入式', '應用', '優化', '智慧', '智能', '進階', '深度學習', '區塊', '分散式']
+CS_ADVANCED_INFO_ANTI_KEY_WORDS = ['asdgladfj;l', '畢業']
 USELESS_COURSES_KEY_WORDS = ['asdgladfj;l']
 USELESS_COURSES_ANTI_KEY_WORDS = ['asdgladfj;l']
 
