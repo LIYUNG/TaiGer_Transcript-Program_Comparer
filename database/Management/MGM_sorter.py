8866d3480d5a6814aa62c74489cfe65c7c904abc
Leo <liyung.chen@tum.de>
2023-05-16 22:38:05 +0200

    with sydney feedback

--- database/Management/MGM_sorter.py
+++ database/Management/MGM_sorter.py
@@ -11,7 +11,7 @@ env_file_path = os.path.realpath(__file__)
 env_file_path = os.path.dirname(env_file_path)
 
 
-def MGM_sorter(program_idx, file_path, abbrev, Generated_Version):
+def MGM_sorter(program_idx, file_path, abbrev, studentId, student_name, analysis_language):
     basic_classification_en = {
         '微積分': [MGM_CALCULUS_KEY_WORDS_EN, MGM_CALCULUS_ANTI_KEY_WORDS_EN],
         '數學': [MGM_MATH_KEY_WORDS_EN, MGM_MATH_ANTI_KEY_WORDS_EN],
@@ -25,8 +25,10 @@ def MGM_sorter(program_idx, file_path, abbrev, Generated_Version):
         '行銷': [MGM_MARKETING_KEY_WORDS_EN, MGM_MARKETING_ANTI_KEY_WORDS_EN],
         '作業研究': [MGM_OP_RESEARCH_KEY_WORDS_EN, MGM_OP_RESEARCH_ANTI_KEY_WORDS_EN],
         '觀察研究': [MGM_EP_RESEARCH_KEY_WORDS_EN, MGM_EP_RESEARCH_ANTI_KEY_WORDS_EN],
+        '基礎資工': [MGM_BASIC_CS_KEY_WORDS_EN, MGM_BASIC_CS_ANTI_KEY_WORDS_EN],
         '程式': [MGM_PROGRAMMING_KEY_WORDS_EN, MGM_PROGRAMMING_ANTI_KEY_WORDS_EN],
         '資料科學': [MGM_DATA_SCIENCE_KEY_WORDS_EN, MGM_DATA_SCIENCE_ANTI_KEY_WORDS_EN],
+        '資訊系統': [MGM_BUSINESS_INFORMATIC_KEY_WORDS_EN, MGM_BUSINESS_INFORMATIC_ANTI_KEY_WORDS_EN],
         '論文': [MGM_BACHELOR_THESIS_KEY_WORDS_EN, MGM_BACHELOR_THESIS_ANTI_KEY_WORDS_EN],
         '其他': [USELESS_COURSES_KEY_WORDS_EN, USELESS_COURSES_ANTI_KEY_WORDS_EN], }
 
@@ -43,10 +45,12 @@ def MGM_sorter(program_idx, file_path, abbrev, Generated_Version):
         '行銷': [MGM_MARKETING_KEY_WORDS, MGM_MARKETING_ANTI_KEY_WORDS],
         '作業研究': [MGM_OP_RESEARCH_KEY_WORDS, MGM_OP_RESEARCH_ANTI_KEY_WORDS],
         '觀察研究': [MGM_EP_RESEARCH_KEY_WORDS, MGM_EP_RESEARCH_ANTI_KEY_WORDS],
+        '基礎資工': [MGM_BASIC_CS_KEY_WORDS, MGM_BASIC_CS_ANTI_KEY_WORDS],
         '程式': [MGM_PROGRAMMING_KEY_WORDS, MGM_PROGRAMMING_ANTI_KEY_WORDS],
         '資料科學': [MGM_DATA_SCIENCE_KEY_WORDS, MGM_DATA_SCIENCE_ANTI_KEY_WORDS],
+        '資訊系統': [MGM_BUSINESS_INFORMATIC_KEY_WORDS, MGM_BUSINESS_INFORMATIC_ANTI_KEY_WORDS],
         '論文': [MGM_BACHELOR_THESIS_KEY_WORDS, MGM_BACHELOR_THESIS_ANTI_KEY_WORDS],
         '其他': [USELESS_COURSES_KEY_WORDS, USELESS_COURSES_ANTI_KEY_WORDS], }
 
     Classifier(program_idx, file_path, abbrev, env_file_path,
-               basic_classification_en, basic_classification_zh, column_len_array, program_sort_function, Generated_Version)
+               basic_classification_en, basic_classification_zh, column_len_array, program_sort_function, studentId, student_name, analysis_language)
